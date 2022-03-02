2904bbe039ec8524f81cdf5fca47780fe8bbf1f4
Wiktor2 <wiktoruszczek@gmail.com>
2022-03-02 18:09:59 +0100

    v0.7

--- src/components/views/Admin/Admin.js
+++ src/components/views/Admin/Admin.js
@@ -1,16 +1,44 @@
+import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { findMatches } from '../../../redux/matchesRedux'
 import { SummaryCard } from '../../features/SummaryCard/SummaryCard'
 import styles from './Admin.module.scss'
+import { Input } from '../../common/Input/Input'
 
 export const Admin = () => {
     const matches = useSelector(state => findMatches(state))
-    return(
-        <>
-            <h1 className={styles.title}>Podgląd statusów</h1>
-            <article className={styles.article}>
-                {matches.map(match => <SummaryCard key={match.id} match={match} />)}
-            </article>
-        </>
-    )
+    const adminName = 'Wiktor'
+    const adminPass = 'olimpia'
+    const [name, setName] = useState('')
+    const [pass, setPass] = useState('')
+    const [log, setLog] = useState(false)
+    const handleSubmit = e => {
+        e.preventDefault();
+        if(name === adminName && pass === adminPass) setLog(true)
+    }
+    if(log) {
+        return(
+            <>
+                <h1 className={styles.title}>Podgląd statusów</h1>
+                <article className={styles.article}>
+                    {matches.map(match => <SummaryCard key={match.id} match={match} />)}
+                </article>
+            </>
+        )
+        }
+    else{
+        return( 
+            <form onSubmit={handleSubmit}>
+            <div>
+                <p>Imię: </p>
+                <Input placeholder={'Podaj swoje imię'} action={e => setName(e.target.value)}/>
+            </div>
+            <div>
+                <p>Hasło: </p>
+                <Input placeholder={'Podaj hasło'} action={e => setPass(e.target.value)}/>
+            </div>
+            <button>Zaloguj</button>
+        </form>
+        )
+    }
 }
