c1236ef40c4031094c034ce300b5c17b59e29092
Wiktor2 <wiktoruszczek@gmail.com>
2022-03-02 14:55:31 +0100

    v0.2

--- src/components/features/LogForm/LogForm.js
+++ src/components/features/LogForm/LogForm.js
@@ -10,21 +10,30 @@ export const LogForm = () => {
     const [name, setName] = useState(undefined);
     const [number, setNumber] = useState(undefined);
     const user = useSelector(state => findUserByNameAndNumber(state, name, number))
-    const url = () =>{
-        if(typeof user === 'object')return '/user' + user.id;
+    const url = () => {
+        if(typeof user === 'object') return '/user' + user.id
         else return '/'
     }
+    const handleSubmit = e => {
+        e.preventDefault();
+        if(typeof user !== 'object') return alert('poprawne') 
+        else {
+            if(user.name === name && user.number === number){
+            } else return alert('Dane do logowania są niepoprawne')
+        }
+    };
+    console.log(url())
     return (
-        <form className={styles.form} action={url()} method='get'>
+        <form className={styles.form} onSubmit={handleSubmit}>
             <div className={styles.box}>
-                <p>Imię:</p>
+                <p>Imię: </p>
                 <Input placeholder={'Podaj swoje imię'} action={e => setName(e.target.value)} />
             </div>
             <div className={styles.box}>
-                <p>Numer:</p>
+                <p>Numer: </p>
                 <Input placeholder={'Podaj swój numer'} action={e => setNumber(e.target.value)} />
             </div>
-            <Button>Zaloguj</Button>
+            <Link to={'/user5'}>Zaloguj się</Link>
         </form>
     )
 }
